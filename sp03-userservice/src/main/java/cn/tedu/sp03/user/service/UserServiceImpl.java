b898af7fa7a47ff2b102f743e0cf48d52ee2beb2
cjl <287817109@qq.com>
2022-03-25 10:18:11 +0800

    update 20220325-1

--- sp03-userservice/src/main/java/cn/tedu/sp03/user/service/UserServiceImpl.java
+++ sp03-userservice/src/main/java/cn/tedu/sp03/user/service/UserServiceImpl.java
@@ -6,10 +6,12 @@ import cn.tedu.web.util.JsonUtil;
 import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@RefreshScope
 @Service
 @Slf4j
 public class UserServiceImpl implements UserService {
